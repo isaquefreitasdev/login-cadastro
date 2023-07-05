271bce2876299140ea0b80ee102d54a16a70faaf
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-07-05 12:17:56 -0300

    organizado

---
+++ controllers/usercontroller.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const User = require('../models/userModel')
+const bcrypt = require("bcryptjs");
+
+
+const controlls = {
+    register:async function (req, res) {
+    try {
+      const userExist = await User.findOne({ email: req.body.email });
+      if (userExist) {
+        return res.status(400).send("Usuário existente");
+      } else {
+        const userRegister = new User({
+          nome: req.body.nome,
+          sobrenome: req.body.sobrenome,
+          email: req.body.email,
+          senha: bcrypt.hashSync(req.body.senha),
+        });
+        const userSave = await userRegister.save();
+        res.status(200).send("Usuário salvo");
+      }
+    } catch (e) {
+      res.status(500).send(e.message);
+    }
+  },
+  getAll:async function (req, res) {
+    try {
+      let users = await User.find();
+      res.send(users);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+}
+
+module.exports = controlls
